44eebaa3036beb27f8b2e4ca97bc5afd10f378b3
adahmo <118616463+adahmo@users.noreply.github.com>
2023-02-03 22:13:47 -0400

    Update master-k8s.sh

--- kubeadm/master-k8s.sh
+++ kubeadm/master-k8s.sh
@@ -104,3 +104,6 @@ su - ubuntu
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
+# to install the pod network 
+kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
+
